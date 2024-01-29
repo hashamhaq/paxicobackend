3a6b14487bce263faf0be5badd90db61baebab06
hashamhaq <155137509+hashamhaq@users.noreply.github.com>
2024-01-29 16:02:25 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 5001;
 
-app.get('/', (req, res) => res.send('Backend 1st change through GIT and container jenkins 5001 version 1.0'));
+app.get('/', (req, res) => res.send('Backend is live'));
 
 app.listen(port, () => console.log(`App 1 listening on port ${port}`));
