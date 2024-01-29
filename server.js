dcdc1276c22784f4f2ecb6ef2ffedbbec8992084
hashamhaq <155137509+hashamhaq@users.noreply.github.com>
2024-01-29 16:43:47 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 5001;
 
-app.get('/', (req, res) => res.send('Backend is live'));
+app.get('/', (req, res) => res.send('Backend is version 1'));
 
 app.listen(port, () => console.log(`App 1 listening on port ${port}`));
